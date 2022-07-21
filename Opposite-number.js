de5cf200a8401cec40446e40e44063173762ab8f
Kashiff <kemist88@gmail.com>
2022-07-21 15:45:58 -0400

    dna to rna

--- Opposite-number.js
+++ Opposite-number.js
@@ -6,3 +6,14 @@
 // 1: -1
 // 14: -14
 // -34: 34
+
+let oppsiteNumber = (num1) => {
+  if (num1 < 0) {
+    console.log(-num1);
+  } else if (num1 > 0) {
+    console.log(-num1);
+  }
+};
+oppsiteNumber(32);
+
+// Solution {  const opposite = number => -number;
